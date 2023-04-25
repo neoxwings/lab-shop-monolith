6267f2af305a36e9bdac0ec4c169f3838957822d
neoxwings <126076295+neoxwings@users.noreply.github.com>
2023-04-25 13:11:24 +0900

    commit

---
+++ frontend/src/components/DeceaseStockCommand.vue
@@ -0,0 +1,62 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            DeceaseStock
+        </v-card-title>
+
+        <v-card-text>
+            <Number label="Qty" v-model="value.qty" :editMode="editMode"/>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="deceaseStock"
+            >
+                DeceaseStock
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'DeceaseStockCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+            this.value.qty = 0;
+        },
+        watch: {
+        },
+        methods: {
+            deceaseStock() {
+                this.$emit('deceaseStock', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
